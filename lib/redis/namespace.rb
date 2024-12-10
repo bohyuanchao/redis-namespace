931d750fc05c6e781815fc2c09088c65adb2c49b
Lulu Du <ldu@medallia.com>
2024-12-10 11:25:01 -0600

    testing

--- lib/redis/namespace.rb
+++ lib/redis/namespace.rb
@@ -61,7 +61,7 @@ class Redis
       "bitpos"           => [ :first ],
       "blpop"            => [ :exclude_last, :first ],
       "brpop"            => [ :exclude_last, :first ],
-      "brpoplpush"       => [ :exclude_last ],
+      "brpoplpush"       => [ :exclude_options ],
       "bzpopmin"         => [ :first ],
       "bzpopmax"         => [ :first ],
       "debug"            => [ :exclude_first ],
